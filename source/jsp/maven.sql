9120765752a6ad1c1029a12110a1731c7b1ef967
SJ <ligerssj@naver.com>
2025-07-03 16:06:16 +0900

    maven 연동 테이블

---
+++ source/jsp/maven.sql
@@ -0,0 +1,48 @@
+-- maven_project 데이터베이스 생성
+CREATE DATABASE maven_project CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+-- 생성된 데이터베이스 사용
+USE maven_project;
+
+-- 회원(member) 테이블 생성
+CREATE TABLE member (
+    member_no INT AUTO_INCREMENT PRIMARY KEY,
+    member_id VARCHAR(50),
+    member_pw VARCHAR(100)
+);
+
+-- 게시판(board) 테이블 생성
+CREATE TABLE board (
+	board_no INT AUTO_INCREMENT PRIMARY KEY,
+  board_title VARCHAR(100) NOT NULL,
+  board_content TEXT,
+  board_writer INT NOT NULL,
+  reg_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  mod_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- 첨부파일(attach) 테이블 생성
+CREATE TABLE attach (
+  attach_no INT AUTO_INCREMENT PRIMARY KEY,
+  board_no INT NOT NULL,                         
+  ori_name VARCHAR(255) NOT NULL,           
+  re_name VARCHAR(255) NOT NULL,             
+  reg_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+SHOW TABLES;
+
+SELECT * FROM member
+
+DESC member;
+
+SELECT board_no ,board_title ,board_content ,board_writer
+			,DATE_FORMAT(reg_date, '%Y-%m-%d') AS reg_date
+			,DATE_FORMAT(mod_date, '%Y-%m-%d') AS mod_date
+		FROM board;
+
+
+SELECT *
+		FROM member AS m
+		JOIN board AS b
+    WHERE	m.member_no = b.board_writer
